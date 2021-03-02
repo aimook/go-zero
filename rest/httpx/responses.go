bd5543e0a9d947bd06638ab6f266434f9112f6d6
aimook <javacspring@hotmail.com>
2021-03-02 18:29:36 +0800

    增加 响应类增加链式调用方法

--- rest/httpx/responses.go
+++ rest/httpx/responses.go
@@ -8,40 +8,115 @@ import (
 	"sync"
 )
 
-var (
-	errorHandler func(error) (int, interface{})
-	lock         sync.RWMutex
-)
-
 // Error writes err into w.
 const (
 	ResponseWrapMessageKey = "message"
 	ResponseWrapCodeKey    = "code"
 	ResponseWrapDataKey    = "data"
 	ResponseWrapSuccessKey = "success"
+	ResponseWrapErrorKey   = "error"
+
+	Model = 1
 )
 
+var (
+	errorHandler func(error) (int, interface{})
+	lock         sync.RWMutex
+	model        int
+)
+
+type reply struct {
+	writer  http.ResponseWriter
+	message string
+	code    int
+	data    interface{}
+	error   error
+}
+
+func Create(w http.ResponseWriter) *reply {
+	return &reply{writer: w}
+}
+
+func (r *reply) Data(data interface{}) *reply {
+	r.data = data
+	return r
+}
+
+func (r *reply) Message(message string) *reply {
+	r.message = message
+	return r
+}
+
+func (r *reply) Code(code int) *reply {
+	r.code = code
+	return r
+}
+
+func (r *reply) Success() {
+	content := make(map[string]interface{}, 0)
+	if "" == r.message {
+		r.message = "success"
+	}
+	content[ResponseWrapMessageKey] = r.message
+	content[ResponseWrapCodeKey] = 10000
+	if nil != r.data {
+		content[ResponseWrapDataKey] = r.data
+	}
+	content[ResponseWrapSuccessKey] = true
+	WriteJson(r.writer, http.StatusOK, content)
+	return
+}
+
+func (r *reply) Error(err error) {
+	r.error = err
+	r.Fail()
+	return
+}
+
+func (r *reply) Fail() {
+	content := make(map[string]interface{}, 0)
+	//如果未定义消息，则尝试从error中获取
+	if "" == r.message {
+		e, ok := r.error.(errorx.BasicError)
+		if ok {
+			//如果未输入状态码，则尝试从error中获取
+			if 0 == r.code {
+				r.code = e.Code
+			}
+			r.message = e.Message
+		} else {
+			r.message = r.error.Error()
+		}
+	}
+	//默认消息
+	if "" == r.message {
+		r.message = "fail"
+	}
+	content[ResponseWrapMessageKey] = r.message
+	//默认状态码
+	if 0 == r.code {
+		r.code = 50000
+	}
+	content[ResponseWrapCodeKey] = r.code
+	if nil != r.data {
+		content[ResponseWrapDataKey] = r.data
+	}
+	content[ResponseWrapSuccessKey] = false
+	if 1 == Model {
+		content[ResponseWrapErrorKey] = r.error
+	}
+	WriteJson(r.writer, http.StatusOK, content)
+	return
+}
+
+// Deprecated: use Create() instead
 func Error(w http.ResponseWriter, err error) {
-	//lock.RLock()
-	//handler := errorHandler
-	//lock.RUnlock()
-	//
-	//if handler == nil {
-	//	WriteJson(w, http.StatusOK, map[string]interface{}{
-	//		ResponseWrapCodeKey:    http.StatusBadRequest,
-	//		ResponseWrapMessageKey: "error handler is null",
-	//		ResponseWrapSuccessKey: false,
-	//	})
-	//	return
-	//}
-	//
-	//code, body := errorHandler(err)
-	e, ok := err.(errorx.Errorable)
-	//e, ok := body.(error)
+	e, ok := err.(errorx.BasicError)
 	if ok {
 		WriteJson(w, http.StatusOK, map[string]interface{}{
 			ResponseWrapMessageKey: e.GetMessage(),
 			ResponseWrapCodeKey:    e.GetCode(),
+			ResponseWrapDataKey:    e.GetData(),
 			ResponseWrapSuccessKey: false,
 		})
 	} else {
@@ -53,11 +128,13 @@ func Error(w http.ResponseWriter, err error) {
 	}
 }
 
+//  Deprecated:  use Create() instead
 // Ok writes HTTP 200 OK into w.
 func Ok(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusOK)
 }
 
+//  Deprecated:  use Create() instead
 // OkJson writes v into w with 200 OK.
 func OkJson(w http.ResponseWriter, v interface{}) {
 	WriteJson(w, http.StatusOK, map[string]interface{}{
@@ -68,11 +145,18 @@ func OkJson(w http.ResponseWriter, v interface{}) {
 	})
 }
 
+//  Deprecated:  use Create() instead
 // SetErrorHandler sets the error handler, which is called on calling Error.
 func SetErrorHandler(handler func(error) (int, interface{})) {
 	lock.Lock()
 	defer lock.Unlock()
 	errorHandler = handler
+}
+
+func SetModel(model int) {
+	lock.Lock()
+	defer lock.Unlock()
+	model = model
 }
 
 // WriteJson writes v as json string into w with code.
