a1b166c0f4f9b58642a4b1a6beca444119472f21
aimook <javacspring@hotmail.com>
2021-03-04 14:36:59 +0800

    增加 MySQL结构化配置参数

--- core/stores/sqlx/mysql.go
+++ core/stores/sqlx/mysql.go
@@ -1,16 +1,31 @@
 package sqlx
 
-import "github.com/go-sql-driver/mysql"
+import (
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+)
 
 const (
 	mysqlDriverName           = "mysql"
 	duplicateEntryCode uint16 = 1062
 )
 
+type DBConfig struct {
+	Host     string
+	Port     int
+	UserName string
+	Passwd   string
+	DBName   string
+}
+
+func (db *DBConfig) toString() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", db.UserName, db.Passwd, db.Host, db.Port, db.DBName)
+}
+
 // NewMysql returns a mysql connection.
-func NewMysql(datasource string, opts ...SqlOption) SqlConn {
+func NewMysql(datasource DBConfig, opts ...SqlOption) SqlConn {
 	opts = append(opts, withMysqlAcceptable())
-	return NewSqlConn(mysqlDriverName, datasource, opts...)
+	return NewSqlConn(mysqlDriverName, datasource.toString(), opts...)
 }
 
 func mysqlAcceptable(err error) bool {
